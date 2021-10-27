7edc3ce597d8543ee83f5b138cbc6e9e98e9c256
AnonymouX47 <feyidab01@gmail.com>
2021-10-27 09:39:37 +0100

    Updated package metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from matrix.__version__ import version
 
 with open("README.md") as f:
     long_desc = f.read()
 
 setup(
-    name="matrix",
+    name="matrix-47",
     version=version,
     author="AnonymouX47",
+    author_email="feyidab01@gmail.com",
     description="A python package for matrix operations and manipulations.",
     long_description=long_desc,
     long_description_content_type="text/markdown",
@@ -20,9 +21,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries",
     ],
-    packages=["matrix"],
+    packages=find_packages("."),
     python_requires=">=3.8",
 )
